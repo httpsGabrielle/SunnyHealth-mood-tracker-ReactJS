bea3c8384f2c354cf97258edb78553334812c58b
httpsGabrielle <gabrielleso125@gmail.com>
2023-09-29 19:07:22 -0300

    update: moodtracker chat emoji

--- src/pages/moodtracker/Notes.js
+++ src/pages/moodtracker/Notes.js
@@ -43,7 +43,7 @@ export default function Notes({note}){
                                     alignItems: 'center'
                                 }}
                             >
-                                <IconProvider icon={note.mood == 'feliz' ? 'mingcute:chat-2-fill' : ''}/>
+                                <IconProvider icon={'mingcute:chat-2-fill'}/>
                             </Box>
                         </Grid>
                         <Grid>
